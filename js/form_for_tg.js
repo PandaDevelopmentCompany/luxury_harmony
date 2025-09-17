a2a278a7546a86064f086f7749edb9ecabc08800
pandadevelopmentcompany <pandadesignstudio@yandex.ru>
2025-09-17 08:36:51 +0300

    download_17_09_2025_1

--- js/form_for_tg.js
+++ js/form_for_tg.js
@@ -1,75 +1,75 @@
-const token = '8142078930:AAHXcNoiP3uNmvkE5ZYuR3yenlXco4lSfDE';
-const chatIds = ['746586393', '5153676171'];
-const URL_API = 'https://api.telegram.org/';
+// const token = '8142078930:AAHXcNoiP3uNmvkE5ZYuR3yenlXco4lSfDE';
+// const chatIds = ['746586393', '5153676171'];
+// const URL_API = 'https://api.telegram.org/';
 
-const messageInput = document.getElementById('messageInput');
-const sendButton = document.getElementById('sendButton');
-const fileInput = document.getElementById('fileInput'); // Добавьте элемент для выбора файла
+// const messageInput = document.getElementById('messageInput');
+// const sendButton = document.getElementById('sendButton');
+// const fileInput = document.getElementById('fileInput'); 
 
-sendButton.addEventListener('click', (event) => {
-    event.preventDefault();
+// sendButton.addEventListener('click', (event) => {
+//     event.preventDefault();
 
-    const message = 'Name: ' + sendName.value + 
-                    '\nPhone number: ' + sendTel.value + 
-                    '\nApplication comment: ' + messageInput.value;
+//     const message = 'Name: ' + sendName.value + 
+//                     '\nPhone number: ' + sendTel.value + 
+//                     '\nApplication comment: ' + messageInput.value;
     
-    let promises = []; // Массив для хранения промисов
+//     let promises = []; 
 
-    chatIds.forEach(chatId => {
-        const formData = new FormData();
-        formData.append('chat_id', chatId);
-        if (fileInput.files.length > 0) {
-            formData.append('caption', message); // Добавляем сообщение как подпись
-            formData.append('document', fileInput.files[0]);
-            promises.push(axios.post(`${URL_API}bot${token}/sendDocument`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            }));
-        } else {
-            promises.push(axios.post(`${URL_API}bot${token}/sendMessage`, {
-                chat_id: chatId,
-                text: message,
-            }));
-        }
-    });
+//     chatIds.forEach(chatId => {
+//         const formData = new FormData();
+//         formData.append('chat_id', chatId);
+//         if (fileInput.files.length > 0) {
+//             formData.append('caption', message); 
+//             formData.append('document', fileInput.files[0]);
+//             promises.push(axios.post(`${URL_API}bot${token}/sendDocument`, formData, {
+//                 headers: {
+//                     'Content-Type': 'multipart/form-data'
+//                 }
+//             }));
+//         } else {
+//             promises.push(axios.post(`${URL_API}bot${token}/sendMessage`, {
+//                 chat_id: chatId,
+//                 text: message,
+//             }));
+//         }
+//     });
 
-    // Ждем, пока все промисы завершатся
-    Promise.all(promises)
-        .then(responses => {
-            console.log(responses);
-            alert('Your message has been successfully sent!✅ \nWe will definitely contact you! \nThank you!💚');
-        })
-        .catch(error => console.error(error));
-});
+   
+//     Promise.all(promises)
+//         .then(responses => {
+//             console.log(responses);
+//             alert('Your message has been successfully sent!✅ \nWe will definitely contact you! \nThank you!💚');
+//         })
+//         .catch(error => console.error(error));
+// });
 
 
 
-const sendName2 = document.getElementById('sendName2');
-const sendTel2 = document.getElementById('sendTel2');
-const messageInput2 = document.getElementById('messageInput2');
-const sendButton2 = document.getElementById('sendButton2');
+// const sendName2 = document.getElementById('sendName2');
+// const sendTel2 = document.getElementById('sendTel2');
+// const messageInput2 = document.getElementById('messageInput2');
+// const sendButton2 = document.getElementById('sendButton2');
 
-sendButton2.addEventListener('click', (event) => {
-    event.preventDefault();
+// sendButton2.addEventListener('click', (event) => {
+//     event.preventDefault();
 
-    const message = 'Name: ' + sendName2.value + 
-                    '\nPhone number: ' + sendTel2.value + 
-                    '\nApplication comment: ' + messageInput2.value;
+//     const message = 'Name: ' + sendName2.value + 
+//                     '\nPhone number: ' + sendTel2.value + 
+//                     '\nApplication comment: ' + messageInput2.value;
 
-    let promises = chatIds.map(chatId => {
-        return axios.post(`${URL_API}bot${token}/sendMessage`, {
-            chat_id: chatId,
-            text: message,
-        });
-    });
+//     let promises = chatIds.map(chatId => {
+//         return axios.post(`${URL_API}bot${token}/sendMessage`, {
+//             chat_id: chatId,
+//             text: message,
+//         });
+//     });
 
-    // Ждем, пока все промисы завершатся
-    Promise.all(promises)
-        .then(responses => {
-            console.log(responses);
-            alert('Your message has been successfully sent!✅ \nWe will definitely contact you! \nThank you!💚');
-        })
-        .catch(error => console.error(error));
-});
+   
+//     Promise.all(promises)
+//         .then(responses => {
+//             console.log(responses);
+//             alert('Your message has been successfully sent!✅ \nWe will definitely contact you! \nThank you!💚');
+//         })
+//         .catch(error => console.error(error));
+// });
 
