1d1c9db032131f51a8bf6aefddda1f2054d5dcae
pandadevelopmentcompany <pandadesignstudio@yandex.ru>
2026-04-27 23:24:52 +0300

    download_27-04_2026-1

--- js/scripts.js
+++ js/scripts.js
@@ -325,6 +325,12 @@ function sendForm(form, hasFile = false) {
             try {
                 await fetch(scriptURL, { method: 'POST', body: new URLSearchParams(payload) });
                 showToast('✅ Your message has been successfully sent!.');
+                            // gtag for google analitycs
+              if (typeof gtag === 'function') {
+        gtag('event', 'conversion', {
+            'send_to': 'AW-18028418774/2iDYCJWQ-Z4cENatz5RD'
+        });
+    }
                 form.reset();
                 resetSliders();
             } catch(err) {
@@ -342,6 +348,13 @@ function sendForm(form, hasFile = false) {
         fetch(scriptURL, { method: 'POST', body: new URLSearchParams(payload) })
         .then(() => {
             showToast('✅ Your message has been successfully sent!.');
+
+            // gtag for google analitycs
+              if (typeof gtag === 'function') {
+        gtag('event', 'conversion', {
+            'send_to': 'AW-18028418774/2iDYCJWQ-Z4cENatz5RD'
+        });
+    }
             form.reset();
             resetSliders();
         })
